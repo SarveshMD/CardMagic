a94e6dc981d1b580c72dbe4a7293dabd3a3d2a06
Sarvesh MD <mdsarvesh1013@gmail.com>
2021-06-19 19:02:55 +0530

    Added colors to outputs

--- app.py
+++ app.py
@@ -1,27 +1,6 @@
 # Importing required module #
 import random
 
-# Setting it up #
-sets = ['Heart', 'Diamond', 'Spade', 'Club']
-numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-cards = []
-for set in sets:
-	for number in numbers:
-		cards.append(f"{set} {number}")
-
-shuffledCards = list()
-shuffledCards.extend(cards)
-random.shuffle(shuffledCards)
-shuffledCards1 = shuffledCards[:21]
-for card in shuffledCards1:
-	print(card)
-
-print("\nChoose a card from the listed cards and write it down somewhere !")
-print("I am going to find what card you chose :)")
-print("Hit enter when you're ready !")
-input()
-print("\n")
-
 # Defining required class #
 # =============================================================== #
 
@@ -37,6 +16,28 @@ class color:
 	UNDERLINE = '\033[4m'
 	END = '\033[0m'
 # =============================================================== #
+
+# Setting it up #
+sets = ['Heart', 'Diamond', 'Spade', 'Club']
+numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+cards = []
+for set in sets:
+	for number in numbers:
+		cards.append(f"{set} {number}")
+
+shuffledCards = list()
+shuffledCards.extend(cards)
+random.shuffle(shuffledCards)
+shuffledCards1 = shuffledCards[:21]
+for card in shuffledCards1:
+	print(color.BLUE + card + color.END)
+
+print(color.CYAN + "\nChoose a card from the listed cards and write it down somewhere !")
+print("I am going to find what card you chose :)")
+print("Hit enter when you're ready !" + color.END)
+input()
+print("\n")
+
 
 # Defining required functions #
 # =============================================================== #
@@ -57,9 +58,9 @@ def splitIntoThree(deck):
 		if num > 3:
 			num = 1
 			deck1.append(card)
-	print("  Deck 1     |   Deck 2      |    Deck 3 ")
+	print("  {}Deck 1     {}|   {}Deck 2      {}|    {}Deck 3 ".format(color.RED, color.END, color.GREEN, color.END, color.BLUE))
 	for card1, card2, card3 in zip(deck1, deck2, deck3):
-		print(card1, " "* (15-len(card1)), card2, " " *(15-len(card2)), card3)
+		print(color.RED + card1, " "* (15-len(card1)), color.GREEN + card2, " " *(15-len(card2)), color.BLUE + card3 + color.END)
 	return deck1, deck2, deck3
 # =============================================================== #
 
@@ -68,10 +69,10 @@ def whereIs(splitNDeck1, splitNDeck2, splitNDeck3):
 	while True:
 		while True:
 			if n>1:
-				print("Invalid deck number ! (re-enter deck number)")
+				print(color.RED + "Invalid deck number ! (re-enter deck number)")
 			else:
 				print()
-				print("Which deck the card you chose is ?\n(Enter the deck number)")
+				print(color.GREEN + "Which deck the card you chose is ?\n(Enter the deck number)")
 			deckNo = input()
 			if deckNo == "1" or deckNo == "2" or deckNo == "3":
 				deckNo = int(deckNo)
@@ -80,21 +81,21 @@ def whereIs(splitNDeck1, splitNDeck2, splitNDeck3):
 				n += 1
 				continue
 		print()
-		print("Are you sure that the card you chose is in this deck ?")
+		print(color.PURPLE + "Are you sure that the card you chose is in this deck ?")
 
 		if deckNo == 1:
 			for card in splitNDeck1:
-				print(card)
+				print(color.RED + card)
 		elif deckNo == 2:
 			for card in splitNDeck2:
-				print(card)
+				print(color.GREEN + card)
 		elif deckNo == 3:
 			for card in splitNDeck3:
-				print(card)
+				print(color.BLUE + card)
 
 		while True:
 			print()
-			confirm = input("(y/n)\n")
+			confirm = input(color.END + "({}y{}/{}n{})\n".format(color.GREEN,color.END,color.RED,color.END))
 			print()
 			if confirm.lower() == "y" or confirm.lower() == "n":
 				break
@@ -165,8 +166,8 @@ shuffledCards4 = orderDecks(shuffledCards4, split3Deck1, split3Deck2, split3Deck
 
 # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-print("The card you chose is :\n")
+print(color.YELLOW + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print(color.BLUE + "The card you chose is :\n")
 
 print(color.RED + f'''
         00000000000           000000000000
@@ -176,7 +177,7 @@ print(color.RED + f'''
   000000                                    0000
   00000                                      0000
  00000                                      00000
- 00000{((37-len(shuffledCards4[10]))//2)*" "}{color.END}{color.CYAN}{shuffledCards4[10]}{color.END}{color.RED}{((37-len(shuffledCards4[10]))//2)*" "}000000
+ 00000{((37-len(shuffledCards4[10]))//2)*" "}{color.CYAN}{shuffledCards4[10]}{color.END}{color.RED}{((37-len(shuffledCards4[10]))//2)*" "}000000
   000000                                 0000000
    0000000                              0000000
      000000                            000000
